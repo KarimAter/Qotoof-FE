572b1c68c4f143c1fbfd421c39a4c97335c89c3a
Karim Ater <karim.abdelbaky1@vodafone.com>
2022-05-17 15:49:05 +0200

    update URL,add butons,styling

--- src/components/Beneficiary.tsx
+++ src/components/Beneficiary.tsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import Button from './Button';
 
 type Props = {name:string}
 
 const Beneficiary = ({ name }) => {
+
+    const deleteBeneficiary = async (name: string) => {
+      await fetch("http://localhost:8000/beneficiary/addBenefciary", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        // body: JSON.stringify({ beneficiaryName }),
+      })
+        .then((res) => {
+          console.log(res);
+          return res.json();
+        })
+        .then((data) =>
+          //,,,
+          console.log("success")
+        )
+        .catch((err) => console.log(err));
+    };
   return (
-    <div className='h-20 border-green-900 border-solid m-4 border-2'>
-      <h2 className='p-2'>{name}</h2>
+    <div className="h-20 border-pink-400 bg-slate-200 border-solid m-4 border-2 flex justify-between">
+      <h2 className=" px-2 font-semibold text-blue-500 my-auto">{name}</h2>
+      <div className="flex my-auto">
+        <Button text={"Edit"} handleClick={() => console.log("edit")}></Button>
+        <Button text={"Delete"} handleClick={() => deleteBeneficiary}></Button>
+      </div>
     </div>
   );
 };
