f2000ba48a43fcf9fa63291beb1776ab74dbc0ae
Karim Ater <karim.abdelbaky1@vodafone.com>
2022-06-28 01:34:11 +0200

    securing donors page

--- src/pages/donors.tsx
+++ src/pages/donors.tsx
@@ -1,16 +1,28 @@
+/* eslint-disable no-shadow */
+
 import { useRouter } from 'next/router';
 import React from 'react';
+import { useSelector } from 'react-redux/es/hooks/useSelector';
 import useSWR from 'swr';
 import Button from '../components/Button';
 import Donor from '../components/Donor';
+import { authSelector } from '../redux/authSlice';
 import API_ENDPOINT from '../utils/constants';
 import { IDonor } from '../utils/interfaces';
 
 type Props = {};
 
 const donors = (props: Props): JSX.Element => {
-  const fetcher = (url: string) => fetch(url).then((res) => res.json());
-  const { data, error } = useSWR<IDonor[]>(`${API_ENDPOINT}/donor/`, fetcher);
+  const { token } = useSelector(authSelector);
+
+  const fetcher = (url: string, token: string) =>
+    fetch(url, { headers: { Authorization: `Bearer ${token}` } }).then((res) =>
+      res.json(),
+    );
+  const { data, error } = useSWR<IDonor[]>(
+    [`${API_ENDPOINT}/donor/`, token],
+    fetcher,
+  );
   const router = useRouter();
   // let output
   const goToForm = () => {
